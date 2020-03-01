ec25fb6ca6f9c34c28bd677e33455f4591112b24
Alexander Hershberger <alex.hershberger@comcast.net>
2020-02-29 20:27:12 -0500

    Test

---
+++ Hello World/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+function newStyle() {
+
+    var elem = document.getElementById('logo');
+    elem.style.color = "purple";
+    elem.style.backgroundColor = "Black";
+    elem.style.fontFamily = "Verdana";
+    elem.innerHTML = "Hello World";
+
+}
